9b990000a16a9cdd8fff77e6675bf615aed389a8
cesar <c@ferrad.as>
2023-01-06 11:39:28 +0000

    add jq and kubectx to setup

--- setup.sh
+++ setup.sh
@@ -4,6 +4,8 @@ echo Installing Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 
 brew install ctags
+brew install jq
+brew install kubectx
 brew install neovim
 brew install postgres
 brew install python
