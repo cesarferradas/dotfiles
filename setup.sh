167c79cafdb77385b7b50315a9dd371c2b11d362
cesar <git@ferrad.as>
2026-05-18 11:00:17 +0900

    Replace p10k theme with robbyrussell

--- setup.sh
+++ setup.sh
@@ -19,7 +19,6 @@ brew install pyright
 echo Setting up Zsh
 chsh -s /bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.11.3
